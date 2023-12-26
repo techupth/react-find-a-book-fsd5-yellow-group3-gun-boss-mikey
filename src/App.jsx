f01c5a41f7c343d2e5bb57f816e24633731b9602
acorriaz <mikey.swx@gmail.com>
2023-12-26 12:54:42 +0700

    Add ex 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [inputText, setInputText] = useState("");
+  const [data, setData] = useState([]);
+
+  console.log("fetch data :", data);
+
+  const fetchFromGoggle = async () => {
+    const result = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${inputText}`
+    );
+    console.log(result);
+    setData(result.data.items);
+  };
+
+  const listElement = data.map((list) => (
+    <li key={list.id}>
+      <a href={list.volumeInfo.infoLink} target="_blank">
+        {list.volumeInfo.title}
+      </a>
+    </li>
+  ));
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      ></input>
+      <button onClick={fetchFromGoggle}>Search</button>
+      <ul>{listElement}</ul>
+    </div>
+  );
 }
 
 export default App;
