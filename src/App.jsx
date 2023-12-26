a9bd3b61f158d92bf30ceba6ad340ab3ccf7e629
Gun <cobaltgun2324@gmail.com>
2023-12-26 15:21:36 +0700

    feat: complete  assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [searchBook, setSearchBook] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+
+  const handleSearch = async () => {
+    const response = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${searchBook}`
+    );
+    console.log(response);
+    setSearchResult(response.data.items);
+  };
+  useEffect(() => {
+    if (searchBook !== "") {
+      handleSearch();
+    }
+  }, [searchBook]);
+
+  return (
+    <div className="App">
+      <div>Find a Book</div>
+      <input
+        className="inputText"
+        type="text"
+        value={searchBook}
+        onChange={(event) => {
+          setSearchBook(event.target.value);
+        }}
+      />
+
+      <div>
+        <ul>
+          {searchResult.map((book, index) => {
+            return <li key={index}>{book.volumeInfo.title}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
